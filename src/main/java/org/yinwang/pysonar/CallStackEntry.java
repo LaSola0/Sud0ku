3adf5fbab2236d094af5bca727582c44f7bc81a8
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2020-10-17 08:35:13 +0000

    rebuild docs

--- src/main/java/org/yinwang/pysonar/CallStackEntry.java
+++ src/main/java/org/yinwang/pysonar/CallStackEntry.java
@@ -1,4 +1,14 @@
 package org.yinwang.pysonar;
 
 import org.yinwang.pysonar.types.FunType;
-import org.yinwang
+import org.yinwang.pysonar.types.Type;
+
+public class CallStackEntry
+{
+    public FunType fun;
+    public Type from;
+
+    public CallStackEntry(FunType fun, Type from)
+    {
+        this.fun = fun;
+     
