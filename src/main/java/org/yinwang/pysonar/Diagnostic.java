5c2ce7e8abd9d2418642c980e632306f4090ee02
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2020-10-17 10:51:42 +0000

    status

--- src/main/java/org/yinwang/pysonar/Diagnostic.java
+++ src/main/java/org/yinwang/pysonar/Diagnostic.java
@@ -5,4 +5,15 @@ import org.jetbrains.annotations.NotNull;
 
 public class Diagnostic {
     public enum Category {
-        INFO, WARNING
+        INFO, WARNING, ERROR
+    }
+
+
+    public String file;
+    public Category category;
+    public int start;
+    public int end;
+    public String msg;
+
+
+    public Diagnostic(String file, 
