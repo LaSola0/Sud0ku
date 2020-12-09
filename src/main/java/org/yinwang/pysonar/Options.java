6d5dceb972c543940f9dc56b060e89dea51475be
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2020-12-09 18:14:44 +0000

    Add webpack

--- src/main/java/org/yinwang/pysonar/Options.java
+++ src/main/java/org/yinwang/pysonar/Options.java
@@ -1,2 +1,10 @@
 package org.yinwang.pysonar;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Options {
+
+    private Map<String, Object> optionsMap = new Link
