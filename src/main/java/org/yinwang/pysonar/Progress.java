d328b18b2c792631345f2c6905830400f4134c42
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-01-02 18:14:14 +0000

    f2fs: reorder nat cache lock in cache_nat_entry

--- src/main/java/org/yinwang/pysonar/Progress.java
+++ src/main/java/org/yinwang/pysonar/Progress.java
@@ -5,4 +5,14 @@ public class Progress {
     private static final int MAX_SPEED_DIGITS = 5;
 
     long startTime;
-    l
+    long lastTickTime;
+    long lastCount;
+    int lastRate;
+    int lastAvgRate;
+    long total;
+    long count;
+    long width;
+    long segSize;
+
+
+    public Progres
