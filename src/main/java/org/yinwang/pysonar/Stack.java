fcc8355a53342c1eee411f683a75f0246bcb5027
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-01-11 16:27:30 +0000

    Updated Main Page

--- src/main/java/org/yinwang/pysonar/Stack.java
+++ src/main/java/org/yinwang/pysonar/Stack.java
@@ -1,4 +1,13 @@
 package org.yinwang.pysonar;
 
 import java.util.ArrayList;
-import java.util
+import java.util.List;
+
+public class Stack<T>
+{
+    private List<T> content = new ArrayList<>();
+
+    public void push(T item)
+    {
+        content.add(item);
+   
