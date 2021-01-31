b8d510a9043bff7591e9b4dbfbc753b83198f3e4
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-01-31 10:36:16 +0000

    deleted file

--- src/main/java/org/yinwang/pysonar/TypeStack.java
+++ src/main/java/org/yinwang/pysonar/TypeStack.java
@@ -4,3 +4,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
+public class TypeStack {
+
+    class Pair {
+        public Object first;
+        public Object second;
+
+
+        public Pair(Object first, Object second) {
