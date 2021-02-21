03e507a24e469f83e1594362ab156203b47bc9cd
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-02-21 17:57:12 +0000

    Prepare 1.4.3 release

--- src/main/java/org/yinwang/pysonar/ast/Attribute.java
+++ src/main/java/org/yinwang/pysonar/ast/Attribute.java
@@ -2,4 +2,11 @@ package org.yinwang.pysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
 
-public class Attribute ex
+public class Attribute extends Node {
+
+    @NotNull
+    public Node target;
+    @NotNull
+    public Name attr;
+
+    public Attribute(@NotNull Node target, @NotNull Name attr, String file, int start, int end, int line,
