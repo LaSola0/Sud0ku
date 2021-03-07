9081fd4c879561c7e4118230af6a0e9eae996ef1
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-03-07 20:33:53 +0000

    README stuff

--- src/main/java/org/yinwang/pysonar/ast/Block.java
+++ src/main/java/org/yinwang/pysonar/ast/Block.java
@@ -1,3 +1,12 @@
 package org.yinwang.pysonar.ast;
 
-import org.jetbrains.an
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class Block extends Node {
+
+    @NotNull
+    public List<Node> seq;
+
+    public Block(@NotNull List<Node> seq, String file, int star
