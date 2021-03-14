917c0c3607af715a2f320226c3cb9b4fa07ec521
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-03-14 14:45:23 +0000

    Update license to MIT

--- src/main/java/org/yinwang/pysonar/ast/Break.java
+++ src/main/java/org/yinwang/pysonar/ast/Break.java
@@ -5,4 +5,12 @@ import org.jetbrains.annotations.NotNull;
 public class Break extends Node {
 
     public Break(String file, int start, int end, int line, int col) {
-        super(NodeType.BREAK, file, start, end,
+        super(NodeType.BREAK, file, start, end, line, col);
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "(break)";
+    }
+}
