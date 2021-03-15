0d6a1c84471200f412c99888474bfe67e9b7e45a
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-03-15 08:08:52 +0000

    v0.7.7

--- src/main/java/org/yinwang/pysonar/ast/Bytes.java
+++ src/main/java/org/yinwang/pysonar/ast/Bytes.java
@@ -1,3 +1,10 @@
 package org.yinwang.pysonar.ast;
 
-import org.jetbrains.a
+import org.jetbrains.annotations.NotNull;
+
+public class Bytes extends Node {
+
+    public Object value;
+
+    public Bytes(@NotNull Object value, String file, int start, int end, int line, int col) {
+      
