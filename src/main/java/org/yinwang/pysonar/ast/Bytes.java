78e48fdd253069e33dfaf5cfdee006bb52a62e83
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-03-15 12:02:42 +0000

    Update WaderTGbot.lua

--- src/main/java/org/yinwang/pysonar/ast/Bytes.java
+++ src/main/java/org/yinwang/pysonar/ast/Bytes.java
@@ -7,4 +7,10 @@ public class Bytes extends Node {
     public Object value;
 
     public Bytes(@NotNull Object value, String file, int start, int end, int line, int col) {
-      
+        super(NodeType.BYTES, file, start, end, line, col);
+        this.value = value.toString();
+    }
+
+    @NotNull
+    @Override
+    public String toStri
