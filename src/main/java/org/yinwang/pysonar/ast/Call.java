34ce4d2bd4f067930f450b9e703f76212bb715ba
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-03-21 14:08:48 +0000

    update graph

--- src/main/java/org/yinwang/pysonar/ast/Call.java
+++ src/main/java/org/yinwang/pysonar/ast/Call.java
@@ -1,4 +1,13 @@
 package org.yinwang.pysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.anno
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class Call extends Node {
+
+    public Node func;
+    public List<Node> args;
+    @Nullable
+    public List<Keyword> k
