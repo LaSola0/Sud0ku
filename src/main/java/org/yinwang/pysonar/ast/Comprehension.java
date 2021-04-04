10ad620b2a5a3dde271bccc19eef449c377857f4
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-04-04 15:22:21 +0000

    mm: process reclaim: vmpressure based process reclaim

--- src/main/java/org/yinwang/pysonar/ast/Comprehension.java
+++ src/main/java/org/yinwang/pysonar/ast/Comprehension.java
@@ -4,4 +4,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class Comprehensio
+public class Comprehension extends Node {
+
+    public Node target;
+    public Node iter;
+    public List<Node> ifs;
+
+    public Comprehension(Node target, Node iter, List<Node> ifs, String file,
