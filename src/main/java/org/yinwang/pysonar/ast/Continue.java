2e6bcf8017696750c95116f5bd0f353404df3991
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-04-11 23:05:20 +0000

    ALSA: usb-audio: work around KEF X300A firmware bug

--- src/main/java/org/yinwang/pysonar/ast/Continue.java
+++ src/main/java/org/yinwang/pysonar/ast/Continue.java
@@ -6,4 +6,12 @@ public class Continue extends Node {
 
     public Continue(String file, int start, int end, int line, int col) {
         super(NodeType.CONTINUE, file, start, end, line, col);
-    
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "(continue)";
+    }
+
+}
