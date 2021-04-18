a6fcfdb8e6089d7acdf2bfb247a6be87c17ebe46
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-04-18 15:01:43 +0000

    update linter

--- src/main/java/org/yinwang/pysonar/ast/DictComp.java
+++ src/main/java/org/yinwang/pysonar/ast/DictComp.java
@@ -7,4 +7,7 @@ import java.util.List;
 public class DictComp extends Node {
 
     public Node key;
-    public Node val
+    public Node value;
+    public List<Comprehension> generators;
+
+    public DictComp(Node key, Node value, List<Comprehension> generators, String file, int start, int end, int line
