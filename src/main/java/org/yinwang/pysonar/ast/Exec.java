13519a33a108c9ad8d1bc3fa0e20c515e2ceb45c
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-04-25 15:36:02 +0000

    Removed IDE files

--- src/main/java/org/yinwang/pysonar/ast/Exec.java
+++ src/main/java/org/yinwang/pysonar/ast/Exec.java
@@ -1,1 +1,11 @@
-package org.yinwang.pyso
+package org.yinwang.pysonar.ast;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Exec extends Node {
+
+    public Node body;
+    public Node globals;
+    public Node locals;
+
+    p
