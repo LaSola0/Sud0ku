6982998447ed03968c6f022ed18c24da99f31361
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-04-25 13:46:38 +0000

    memory fixes

--- src/main/java/org/yinwang/pysonar/ast/Ellipsis.java
+++ src/main/java/org/yinwang/pysonar/ast/Ellipsis.java
@@ -5,4 +5,13 @@ import org.jetbrains.annotations.NotNull;
 public class Ellipsis extends Node {
 
     public Ellipsis(String file, int start, int end, int line, int col) {
-        super(NodeT
+        super(NodeType.ELLIPSIS, file, start, end, line, col);
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "...";
+    }
+
+}
