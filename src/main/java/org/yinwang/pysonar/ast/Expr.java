e1df6530ea96df4bf27de2dfdeeb38bd6a42ac72
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-05-09 11:13:27 +0000

    Update events.yml

--- src/main/java/org/yinwang/pysonar/ast/Expr.java
+++ src/main/java/org/yinwang/pysonar/ast/Expr.java
@@ -10,4 +10,15 @@ public class Expr extends Node {
     public Node value;
 
     public Expr(Node n, String file, int start, int end, int line, int col) {
-        super(NodeType.EXPR, file, start, end, line, col
+        super(NodeType.EXPR, file, start, end, line, col);
+        this.value = n;
+        addChildren(n);
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "<Expr:" + value + ">";
+    }
+
+}
