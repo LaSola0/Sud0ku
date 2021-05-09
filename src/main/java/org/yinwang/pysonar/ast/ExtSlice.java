23becb618e61807fb93983ce5a81007854e16830
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-05-09 11:41:18 +0000

    Update index-ja.md

--- src/main/java/org/yinwang/pysonar/ast/ExtSlice.java
+++ src/main/java/org/yinwang/pysonar/ast/ExtSlice.java
@@ -1,1 +1,11 @@
-package org.yi
+package org.yinwang.pysonar.ast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ExtSlice extends Node {
+
+    public List<Node> dims;
+
+    public 
