9b5a7bf20e7f78ea4866477f8efa57cd6fac803f
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-05-23 06:47:17 +0000

    * Sync with the trunk.

--- src/main/java/org/yinwang/pysonar/ast/ExtSlice.java
+++ src/main/java/org/yinwang/pysonar/ast/ExtSlice.java
@@ -10,4 +10,12 @@ public class ExtSlice extends Node {
 
     public ExtSlice(List<Node> dims, String file, int start, int end, int line, int col) {
         super(NodeType.EXTSLICE, file, start, end, line, col);
-      
+        this.dims = dims;
+        addChildren(dims);
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "<ExtSlice:" + dims + ">";
+   
