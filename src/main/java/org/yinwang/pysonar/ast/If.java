f665a1fcd3b8c58da7b57ad79cb2c02747a002b2
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-06-02 05:36:49 +0000

    netfilter: xt_qtaguid: remove AID_* dependency for access control

--- src/main/java/org/yinwang/pysonar/ast/If.java
+++ src/main/java/org/yinwang/pysonar/ast/If.java
@@ -2,4 +2,11 @@ package org.yinwang.pysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
 
-public class If extends N
+public class If extends Node {
+
+    @NotNull
+    public Node test;
+    public Node body;
+    public Node orelse;
+
+    public If(@NotNull Node test, Node body, Node orelse, String file
