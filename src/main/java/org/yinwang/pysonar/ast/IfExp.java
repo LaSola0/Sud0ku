14700692376e2227d97e09ea088852122e78009f
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-06-13 20:27:23 +0000

    [dummypythonqt] Automatic merge of Transifex translations

--- src/main/java/org/yinwang/pysonar/ast/IfExp.java
+++ src/main/java/org/yinwang/pysonar/ast/IfExp.java
@@ -6,4 +6,7 @@ public class IfExp extends Node {
 
     public Node test;
     public Node body;
-   
+    public Node orelse;
+
+    public IfExp(Node test, Node body, Node orelse, String file, int start, int end, int line, int col) {
+        super(NodeType.IFEXP, file, start, e
