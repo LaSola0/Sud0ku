5a386143b2752c4ea6025d09a2264ac74a372578
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-06-29 18:30:49 +0000

    change version

--- src/main/java/org/yinwang/pysonar/ast/Import.java
+++ src/main/java/org/yinwang/pysonar/ast/Import.java
@@ -1,1 +1,11 @@
-package org.yinwang.pysonar.
+package org.yinwang.pysonar.ast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class Import extends Node {
+
+    public List<Alias> names;
+
+    public Import(List<Alias> names, String file, int s
