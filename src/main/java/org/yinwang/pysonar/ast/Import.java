1bebb27819891753e181aabe92100b71597e8816
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-06-30 09:43:31 +0000

    Update docker

--- src/main/java/org/yinwang/pysonar/ast/Import.java
+++ src/main/java/org/yinwang/pysonar/ast/Import.java
@@ -8,4 +8,10 @@ public class Import extends Node {
 
     public List<Alias> names;
 
-    public Import(List<Alias> names, String file, int s
+    public Import(List<Alias> names, String file, int start, int end, int line, int col) {
+        super(NodeType.IMPORT, file, start, end, line, col);
+        this.names = names;
+        addChildren(names);
+    }
+
+    @No
