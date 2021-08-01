d222e82bb5ee80303f8e2bb2c2bee69f32865d9b
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-08-01 05:56:35 +0000

    Fix setup.py

--- src/main/java/org/yinwang/pysonar/ast/ImportFrom.java
+++ src/main/java/org/yinwang/pysonar/ast/ImportFrom.java
@@ -85,4 +85,9 @@ public class ImportFrom extends Node {
     }
 
     @NotNull
-    @Overrid
+    @Override
+    public String toString() {
+        return "<FromImport:" + module + ":" + names + ">";
+    }
+
+}
