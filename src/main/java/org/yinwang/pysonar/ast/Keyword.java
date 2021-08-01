edd6198c5ee1d79aff6d15a1721fc684decef0c7
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-08-01 06:11:47 +0000

    Reformatted.

--- src/main/java/org/yinwang/pysonar/ast/Keyword.java
+++ src/main/java/org/yinwang/pysonar/ast/Keyword.java
@@ -1,3 +1,12 @@
 package org.yinwang.pysonar.ast;
 
-i
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a keyword argument (name=value) in a function call.
+ */
+public class Keyword extends Node {
+
+    public String arg;
+    @NotNull
+  
