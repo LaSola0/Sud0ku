91d67226812249c9d289a8cd5e66ad238a395938
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-08-08 22:48:36 +0000

    Initialize repository

--- src/main/java/org/yinwang/pysonar/ast/Keyword.java
+++ src/main/java/org/yinwang/pysonar/ast/Keyword.java
@@ -9,4 +9,7 @@ public class Keyword extends Node {
 
     public String arg;
     @NotNull
-  
+    public Node value;
+
+    public Keyword(String arg, @NotNull Node value, String file, int start, int end, int line, int col) {
+        super(NodeType.KEYWORD, file, 
