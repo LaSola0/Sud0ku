6018fe147fd67ef43aa343cffc48156a86ff4ce3
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-08-20 18:37:44 +0000

    Delete build.gradle

--- src/main/java/org/yinwang/pysonar/ast/NodeType.java
+++ src/main/java/org/yinwang/pysonar/ast/NodeType.java
@@ -1,1 +1,14 @@
-pa
+package org.yinwang.pysonar.ast;
+
+public enum NodeType {
+    ALIAS,
+    ASSERT,
+    ASSIGN,
+    ATTRIBUTE,
+    AWAIT,
+    BINOP,
+    BLOCK,
+    BREAK,
+    BYTES,
+    CALL,
+    CLA
