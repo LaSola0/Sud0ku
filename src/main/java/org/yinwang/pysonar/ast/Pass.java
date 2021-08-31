0b111d13813755a1aed30950f25ee2fbe7fd72f9
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-08-31 08:48:05 +0000

    gen_stats.c: Duplicate xstats buffer for later use

--- src/main/java/org/yinwang/pysonar/ast/Pass.java
+++ src/main/java/org/yinwang/pysonar/ast/Pass.java
@@ -5,4 +5,13 @@ import org.jetbrains.annotations.NotNull;
 public class Pass extends Node {
 
     public Pass(String file, int start, int end, int line, int col) {
-        super(NodeType.PASS, file, 
+        super(NodeType.PASS, file, start, end, line, col);
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "<Pass>";
+    }
+
+}
