e38310402502e26d2cb5a2548aaee3eb046720b2
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-09-05 19:20:59 +0000

    Wording change

--- src/main/java/org/yinwang/pysonar/ast/PyComplex.java
+++ src/main/java/org/yinwang/pysonar/ast/PyComplex.java
@@ -10,4 +10,13 @@ public class PyComplex extends Node {
     public PyComplex(double real, double imag, String file, int start, int end, int line, int col) {
         super(NodeType.PYCOMPLEX, file, start, end, line, col);
         this.real = real;
-   
+        this.imag = imag;
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "(" + real + "+" + imag + "j)";
+    }
+
+}
