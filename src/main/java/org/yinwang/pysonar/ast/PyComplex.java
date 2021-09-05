c94cc20a72d7b1074b6dff942f29fd3a4d9fc652
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-09-05 09:52:12 +0000

    improved testing

--- src/main/java/org/yinwang/pysonar/ast/PyComplex.java
+++ src/main/java/org/yinwang/pysonar/ast/PyComplex.java
@@ -1,3 +1,10 @@
 package org.yinwang.pysonar.ast;
 
-impo
+import org.jetbrains.annotations.NotNull;
+
+public class PyComplex extends Node {
+
+    public double real;
+    public double imag;
+
+    public PyComplex(double real, double imag, Stri
