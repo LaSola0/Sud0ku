292e5f53edc1ed2d6de1bd25e6934dcfae30b116
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-09-08 09:53:52 +0000

    missing dependencies

--- src/main/java/org/yinwang/pysonar/ast/PyInt.java
+++ src/main/java/org/yinwang/pysonar/ast/PyInt.java
@@ -1,1 +1,11 @@
-packa
+package org.yinwang.pysonar.ast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigInteger;
+
+public class PyInt extends Node {
+
+    public BigInteger value;
+
+    publ
