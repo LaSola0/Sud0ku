554f5854296c6e3f3ab8d2960406c98388951ceb
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-09-26 21:41:11 +0000

    [maven-release-plugin][skip ci] prepare for next development iteration

--- src/main/java/org/yinwang/pysonar/ast/PyList.java
+++ src/main/java/org/yinwang/pysonar/ast/PyList.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public class PyList extends Sequence {
 
-    public PyList(@NotNull List<Node> elts, String file, int start, i
+    public PyList(@NotNull List<Node> elts, String file, int start, int end, int line, int col) {
+        super(NodeType.PYLIST, elts, file, start, end, line, col);
+    }
+
+    @NotNull
+    @Override
+    public String toString() 
