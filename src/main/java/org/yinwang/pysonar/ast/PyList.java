1201a11854251cf6de61c4e8224eb0b904f4ca43
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-09-26 08:46:55 +0000

    v2.2.3

--- src/main/java/org/yinwang/pysonar/ast/PyList.java
+++ src/main/java/org/yinwang/pysonar/ast/PyList.java
@@ -1,3 +1,9 @@
 package org.yinwang.pysonar.ast;
 
-import
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class PyList extends Sequence {
+
+    public PyList(@NotNull List<Node> elts, String file, int start, i
