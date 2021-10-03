72c20939efd92f9ff8290e5b79b2996919409eed
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-10-03 07:20:28 +0000

    Added date

--- src/main/java/org/yinwang/pysonar/ast/PyModule.java
+++ src/main/java/org/yinwang/pysonar/ast/PyModule.java
@@ -1,4 +1,11 @@
 package org.yinwang.pysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
-import org.y
+import org.yinwang.pysonar.$;
+
+public class PyModule extends Node {
+
+    public Block body;
+
+    public PyModule(Block body, String file, int start, int end, int line, int col) {
+      
