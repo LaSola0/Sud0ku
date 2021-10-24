c491d15fa49bb2a378633d553fd12ff27fe4d8d4
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-10-24 06:40:00 +0000

    alpha

--- src/main/java/org/yinwang/pysonar/ast/Starred.java
+++ src/main/java/org/yinwang/pysonar/ast/Starred.java
@@ -2,4 +2,9 @@ package org.yinwang.pysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
 
-public class
+public class Starred extends Node {
+
+    public Node value;
+
+    public Starred(Node n, String file, int start, int end, int line, int col) {
+  
