1c0bbfc37823eaad2a2a502afad62b2d59f73cc6
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-10-27 19:22:06 +0000

    Fix silly typo

--- src/main/java/org/yinwang/pysonar/ast/Str.java
+++ src/main/java/org/yinwang/pysonar/ast/Str.java
@@ -2,4 +2,10 @@ package org.yinwang.pysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
 
-public class Str extend
+public class Str extends Node {
+
+    public String value;
+
+    public Str(@NotNull Object value, String file, int start, int end, int line, int col) {
+        super(NodeType.STR, file, start, end, line, col);
+   
