5a8a24e745a46af09322acf20b1e0d969c19b856
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-11-10 20:46:29 +0000

    GTFS Direction Finder > enabled

--- src/main/java/org/yinwang/pysonar/ast/UnaryOp.java
+++ src/main/java/org/yinwang/pysonar/ast/UnaryOp.java
@@ -8,4 +8,12 @@ public class UnaryOp extends Node {
     public Node operand;
 
     public UnaryOp(Op op, Node operand, String file, int start, int end, int line, int col) {
-        super(NodeType.UNARYOP, file, start, end, line
+        super(NodeType.UNARYOP, file, start, end, line, col);
+        this.op = op;
+        this.operand = operand;
+        addChildren(operand);
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
