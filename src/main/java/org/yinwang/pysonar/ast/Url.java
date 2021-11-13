c5dfae33c4f7cb9f5816760682bafe74203dff09
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-11-13 10:05:38 +0000

    update methods

--- src/main/java/org/yinwang/pysonar/ast/Url.java
+++ src/main/java/org/yinwang/pysonar/ast/Url.java
@@ -1,3 +1,13 @@
 package org.yinwang.pysonar.ast;
 
-import org.jetbrains.annotations.NotN
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * virtual-AST node used to represent virtual source locations for builtins
+ * as external urls.
+ */
+public class Url extends Node {
+
+    public String url;
+
+    public Url(S
