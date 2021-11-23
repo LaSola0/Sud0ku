3b825b3f9c7ff43cdfc565fc984e8dbc09a2e4fe
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-11-23 16:48:03 +0000

    ixp4xx_hss: fix build failure due to missing linux/module.h inclusion

--- src/main/java/org/yinwang/pysonar/demos/Demo.java
+++ src/main/java/org/yinwang/pysonar/demos/Demo.java
@@ -108,4 +108,7 @@ public class Demo {
             return "";
         }
 
-        List<Style> styles = new ArrayList<>(linker.getStyles(p
+        List<Style> styles = new ArrayList<>(linker.getStyles(path));
+
+        String styledSource = new StyleApplier(path, source, styles).apply();
+        String outline = new HtmlOutline(analyzer).generate(p
