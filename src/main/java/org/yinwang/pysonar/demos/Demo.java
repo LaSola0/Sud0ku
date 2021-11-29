d7fdee85b2b0a45441dfcc7c559bdd4005fe93d6
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-11-29 10:34:56 +0000

    corrected path

--- src/main/java/org/yinwang/pysonar/demos/Demo.java
+++ src/main/java/org/yinwang/pysonar/demos/Demo.java
@@ -139,4 +139,7 @@ public class Demo {
         StringBuilder result = new StringBuilder((int) (source.length() * 1.2));
         int count = 1;
         for (String line : source.split("\n")) {
-            result.append("<span class='linen
+            result.append("<span class='lineno'>");
+            result.append(String.format("%1$4d", count++));
+            result.append("</span> ");
+            result.append(line);
