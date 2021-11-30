7558896d7a491b40f0f74bdff3daf6b579bc82af
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-11-30 19:03:30 +0000

    Setting version to 2.0.0

--- src/main/java/org/yinwang/pysonar/demos/Style.java
+++ src/main/java/org/yinwang/pysonar/demos/Style.java
@@ -1,3 +1,11 @@
 package org.yinwang.pysonar.demos;
 
-import org.jetbrains.annotatio
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.yinwang.pysonar.$;
+
+import java.util.List;
+
+
+/**
+ * Represents a simple style run for purposes of source highlight
