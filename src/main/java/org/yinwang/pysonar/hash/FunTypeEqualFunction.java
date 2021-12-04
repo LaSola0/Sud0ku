a8f408291f5a69796fee3da8cccc69759a4025f3
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-12-04 21:13:24 +0000

    net: qmi_wwan: add ZTE MF60

--- src/main/java/org/yinwang/pysonar/hash/FunTypeEqualFunction.java
+++ src/main/java/org/yinwang/pysonar/hash/FunTypeEqualFunction.java
@@ -3,4 +3,9 @@ package org.yinwang.pysonar.hash;
 import org.yinwang.pysonar.types.FunType;
 
 
-public class FunTypeEqualFunction e
+public class FunTypeEqualFunction extends EqualFunction {
+
+    @Override
+    public boolean equals(Object x, Object y) {
+        if (x instanceof FunType && y instanceof FunType) {
+            FunType 
