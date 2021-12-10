311a004c455d3635da0ba08746511c8dd3cb45bc
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-12-10 09:43:30 +0000

    Create python-package.yml

--- src/main/java/org/yinwang/pysonar/types/ClassType.java
+++ src/main/java/org/yinwang/pysonar/types/ClassType.java
@@ -15,4 +15,8 @@ public class ClassType extends Type {
     public Type superclass;
     private InstanceType instance;
 
-    public ClassType(@NotNull String name, @
+    public ClassType(@NotNull String name, @Nullable State parent) {
+        this.name = name;
+        this.setTable(new State(parent, State.StateType.CLASS));
+        table.setType(this);
+        if (parent != nul
