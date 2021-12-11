216bde21e36eb14b8bcc55ea3e8c7eec7b6877c3
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-12-11 14:49:39 +0000

    test improved

--- src/main/java/org/yinwang/pysonar/types/DictType.java
+++ src/main/java/org/yinwang/pysonar/types/DictType.java
@@ -7,4 +7,8 @@ public class DictType extends Type {
     public Type keyType;
     public Type valueType;
 
-    public DictType(Type key
+    public DictType(Type key0, Type val0) {
+        keyType = key0;
+        valueType = val0;
+        table.addSuper(Types.BaseDict.table);
+        table.setPath(Types.BaseDict
