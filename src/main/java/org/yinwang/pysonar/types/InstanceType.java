2103992cd37a989eb2555c6489d366aca5698bfc
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-12-15 14:13:00 +0000

    Work on sphinx docs.

--- src/main/java/org/yinwang/pysonar/types/InstanceType.java
+++ src/main/java/org/yinwang/pysonar/types/InstanceType.java
@@ -3,4 +3,14 @@ package org.yinwang.pysonar.types;
 import org.jetbrains.annotations.NotNull;
 import org.yinwang.pysonar.State;
 import org.yinwang.pysonar.ast.Node;
-import org.yinwang.
+import org.yinwang.pysonar.visitor.TypeInferencer;
+
+import java.util.List;
+
+
+public class InstanceType extends Type {
+
+    public Type classType;
+
+
+    public InstanceType(@NotNull
