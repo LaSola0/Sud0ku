23ff419dfad1ff0ed05336676542f7c2518fe292
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-12-17 08:34:24 +0000

    update trello integration

--- src/main/java/org/yinwang/pysonar/types/ListType.java
+++ src/main/java/org/yinwang/pysonar/types/ListType.java
@@ -1,1 +1,10 @@
-packa
+package org.yinwang.pysonar.types;
+
+import org.jetbrains.annotations.NotNull;
+import org.yinwang.pysonar.Analyzer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListTy
