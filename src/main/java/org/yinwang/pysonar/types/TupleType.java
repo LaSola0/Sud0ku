828a9d15ba63f95f73f56cf7ddbb6a11a29c0fce
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2021-12-21 14:15:26 +0000

    updating bower.json

--- src/main/java/org/yinwang/pysonar/types/TupleType.java
+++ src/main/java/org/yinwang/pysonar/types/TupleType.java
@@ -5,4 +5,14 @@ import org.yinwang.pysonar.Analyzer;
 import org.yinwang.pysonar.TypeStack;
 
 import java.util.ArrayList;
-import java.
+import java.util.Collections;
+import java.util.List;
+
+
+public class TupleType extends Type {
+
+    public List<Type> eltTypes;
+
+
+    public TupleType() {
+        this.eltT
