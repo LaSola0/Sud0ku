2cf4680542a22282713b26de6f85220b1e3464c7
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2022-01-07 06:21:08 +0000

    frv: Add missing RCU idle APIs on idle loop

--- src/main/java/org/yinwang/pysonar/types/UnionType.java
+++ src/main/java/org/yinwang/pysonar/types/UnionType.java
@@ -1,4 +1,15 @@
 package org.yinwang.pysonar.types;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class UnionType extends Type {
+
+    public Set<Type> types;
+
+
+    public UnionType() {
+        th
