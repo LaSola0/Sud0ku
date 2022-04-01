c1b90e2fa4bc57b532ba0cabde64a976595ee129
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2022-04-01 14:05:10 +0000

    Reconcile with jb-release nakasi-factoryrom-release

--- src/main/java/org/yinwang/pysonar/visitor/TypeInferencer.java
+++ src/main/java/org/yinwang/pysonar/visitor/TypeInferencer.java
@@ -1114,4 +1114,11 @@ public class TypeInferencer implements Visitor1<Type, State>
         State s1 = s.copy();
         State s2 = s.copy();
 
-   
+        if (node.body != null)
+        {
+            t1 = visit(node.body, s1);
+            s.merge(s1);
+
+            t2 = visit(node.body, s1);
+            s.merge(s1);
+        
