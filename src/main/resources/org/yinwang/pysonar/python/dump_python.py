4454c030937215011a0e14d804be08d8b8d6e31e
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2022-06-19 10:25:06 +0000

    fixed some memory leaks

--- src/main/resources/org/yinwang/pysonar/python/dump_python.py
+++ src/main/resources/org/yinwang/pysonar/python/dump_python.py
@@ -95,3 +95,190 @@ def detect_encoding(path):
         enc_len = len(decl[0])
         try:
             codecs.lookup(enc1)
+        except LookupError:
+            return 'latin1', enc_len
+        return enc1, enc_len
+    else:
+        return 'latin1', -1
+
+
+#-------------------------------------------------------------
+#                   improvements to the AST
+#-------------------------------------------------------------
+def improve_ast(node, s):
+    build_index_map(s)
+    improve_node(node, s)
+
+
+line_starts = []
+
+
+# build global table 'idxmap' for lineno <-> index conversion
+def build_index_map(s):
+    global line_starts
+    idx = 0
+    line_starts = [0]
+    while idx < len(s):
+        if s[idx] == '\n':
+            line_starts.append(idx + 1)
+        idx += 1
+
+
+# convert (line, col) to offset index
+def map_idx(line, col):
+    return line_starts[line - 1] + col
+
+
+# convert offset index into (line, col)
+def map_line_col(idx):
+    line = 0
+    for start in line_starts:
+        if idx < start:
+            break
+        line += 1
+    col = idx - line_starts[line - 1]
+    return line, col
+
+
+def improve_node(node, s):
+    if isinstance(node, list):
+        for n in node:
+            improve_node(n, s)
+
+    elif isinstance(node, AST):
+        find_start(node, s)
+        find_end(node, s)
+        if hasattr(node, 'start'):
+            node.lineno, node.col_offset = map_line_col(node.start)
+        add_missing_names(node, s)
+
+        for f in node_fields(node):
+            improve_node(f, s)
+
+
+def find_start(node, s):
+    ret = None    # default value
+
+    if hasattr(node, 'start'):
+        ret = node.start
+
+    elif isinstance(node, list):
+        if node != []:
+            ret = find_start(node[0], s)
+
+    elif isinstance(node, Module):
+        if node.body != []:
+            ret = find_start(node.body[0], s)
+
+    elif isinstance(node, BinOp):
+        leftstart = find_start(node.left, s)
+        if leftstart is not None:
+            ret = leftstart
+        else:
+            ret = map_idx(node.lineno, node.col_offset)
+
+    elif hasattr(node, 'lineno'):
+        if node.col_offset >= 0:
+            ret = map_idx(node.lineno, node.col_offset)
+        else:                           # special case for """ strings
+            i = map_idx(node.lineno, node.col_offset)
+            while i > 0 and i + 2 < len(s) and s[i:i + 3] != '"""' and s[i:i + 3] != "'''":
+                i -= 1
+            ret = i
+    else:
+        return None
+
+    if ret is None and hasattr(node, 'lineno'):
+        raise TypeError("got None for node that has lineno", node)
+
+    if isinstance(node, AST) and ret is not None:
+        node.start = ret
+
+    return ret
+
+
+def find_end(node, s):
+    the_end = None
+
+    if hasattr(node, 'end'):
+        return node.end
+
+    elif isinstance(node, list):
+        if node != []:
+            the_end = find_end(node[-1], s)
+
+    elif isinstance(node, Module):
+        if node.body != []:
+            the_end = find_end(node.body[-1], s)
+
+    elif isinstance(node, Expr):
+        the_end = find_end(node.value, s)
+
+    elif isinstance(node, Str):
+        i = find_start(node, s)
+        while s[i] != '"' and s[i] != "'":
+            i += 1
+
+        if i + 2 < len(s) and s[i:i + 3] == '"""':
+            q = '"""'
+            i += 3
+        elif i + 2 < len(s) and s[i:i + 3] == "'''":
+            q = "'''"
+            i += 3
+        elif s[i] == '"':
+            q = '"'
+            i += 1
+        elif s[i] == "'":
+            q = "'"
+            i += 1
+        else:
+            print("illegal quote:", i, s[i])
+            q = ''
+
+        if q != '':
+            the_end = end_seq(s, q, i)
+
+    elif isinstance(node, Name):
+        the_end = find_start(node, s) + len(node.id)
+
+    elif isinstance(node, Attribute):
+        the_end = end_seq(s, node.attr, find_end(node.value, s))
+
+    elif isinstance(node, FunctionDef) or (python3 and isinstance(node, AsyncFunctionDef)):
+        the_end = find_end(node.body, s)
+
+    elif isinstance(node, Lambda):
+        the_end = find_end(node.body, s)
+
+    elif isinstance(node, ClassDef):
+        the_end = find_end(node.body, s)
+
+    # print will be a Call in Python 3
+    elif not python3 and isinstance(node, Print):
+        the_end = start_seq(s, '\n', find_start(node, s))
+
+    elif isinstance(node, Call):
+        start = find_end(node.func, s)
+        if start is not None:
+            the_end = match_paren(s, '(', ')', start)
+
+    elif isinstance(node, Yield):
+        the_end = find_end(node.value, s)
+
+    elif isinstance(node, Return):
+        if node.value is not None:
+            the_end = find_end(node.value, s)
+        else:
+            the_end = find_start(node, s) + len('return')
+
+    elif (isinstance(node, For) or
+          isinstance(node, While) or
+          isinstance(node, If) or
+          isinstance(node, IfExp)):
+        if node.orelse != []:
+            the_end = find_end(node.orelse, s)
+        else:
+            the_end = find_end(node.body, s)
+
+    elif isinstance(node, Assign) or isinstance(node, AugAssign):
+        the_end = find_end(node.value, s)
