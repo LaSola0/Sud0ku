79ab9a5550032e8df7059d2aaa967c1dfb0c6dae
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2022-07-24 15:17:20 +0000

    update README.md

--- tests/element-type.test/test1.py
+++ tests/element-type.test/test1.py
@@ -1,1 +1,14 @@
-# test i
+# test inferring element type from subscript assignment, and append, update methods
+
+a = []
+a[0] = 1
+print a
+
+b = {}
+b[0] = "hello"
+x = b[1]
+print x
+
+c = []
+c.append(1)
+z = c[0]
