f42603a20ea75389fc0553beac3c0d6efb89f9aa
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2022-08-25 22:24:57 +0000

    djy

--- tests/import/import-from.test/import-same-level.py
+++ tests/import/import-from.test/import-same-level.py
@@ -1,4 +1,4 @@
 from drawer import Knife
 
 k1 = Knife()
-print k1
+print k1.length
---
+++ tests/import/import-from.test/import-star-from-init.py
@@ -0,0 +1,9 @@
+# import * from __init__.py of kitchen package
+
+from kitchen import *
+
+print spoon(1)
+print fork(2)
+
+# knife is not export from __init__.py
+# should not be able to fi
