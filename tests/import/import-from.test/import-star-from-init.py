4f142f5fc43ab188a8926726142b61a748ce5e00
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2022-08-28 05:48:12 +0000

    NCAA women's volleyball update.

--- tests/import/import-from.test/import-star-from-init.py
+++ tests/import/import-from.test/import-star-from-init.py
@@ -6,4 +6,5 @@ print spoon(1)
 print fork(2)
 
 # knife is not export from __init__.py
-# should not be able to fi
+# should not be able to find it
+print knife(3)
---
+++ tests/import/import-from.test/import-star-from-oven.py
@@ -0,0 +1,10 @@
+# import * from module oven.py
+
+from kitchen.oven import *
+
+pizza = Pizza()
+print(pizza.size)
+
+bread1 = Bread()
+size1 = bread1.size
+bread2 = Bread
