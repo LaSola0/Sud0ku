fc319473fd3a4be0e76bc8cd4afb84e4d3aff303
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2023-02-05 17:19:57 +0000

    Convert to manifest_version 2

--- tests/recursion.test/test1.py
+++ tests/recursion.test/test1.py
@@ -4,4 +4,15 @@ class Base:
 
 class A(Base):
     def x(self):
-    
+        return "A"
+
+class B(Base):
+    def x(Base):
+        return "B"
+
+def f1(n):
+    if n == 0:
+        a = A()
+        return a
+    else:
+        return f1(0).x()
