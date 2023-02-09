991a74778d052a32f56a181d43deadfe1497c527
LaSola0 <71031558+LaSola0@users.noreply.github.com>
2023-02-09 07:13:40 +0000

    log error

--- tests/redefine-op.test/redefine-plus.py
+++ tests/redefine-op.test/redefine-plus.py
@@ -7,4 +7,14 @@ class A:
 
     def __sub__(self, other):
         if isinstance(other, int):
-            return 2 * othe
+            return 2 * other
+
+    def __lt__(self, other):
+        if isinstance(other, int):
+            return len(str(self.value)) < other
+        else:
+            return False
+
+
+def fib(n):
+    if n < 2
